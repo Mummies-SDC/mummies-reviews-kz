f10121343178750bfdbf5ead483d6e01c1d0f468
Kevin Zhang <kevz.workspace@gmail.com>
2022-11-05 09:40:30 -0700

    Added routes for all except meta data

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool, Client } = require('pg')
 
 const client = new Client({
-  user: 'kevinz',
-  host: 'localhost',
-  database: 'product_reviews_database',
-  password: '',
-  port: 5432,
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
 });
 
 client.connect();
